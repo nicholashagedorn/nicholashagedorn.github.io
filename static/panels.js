7d7a4c1a7e9742bf90ceb35472df0b074054bebe
Nick Hagedorn <nicholas.d.hagedorn@gmail.com>
2022-10-08 10:45:09 -0400

    Changed links

--- static/panels.js
+++ static/panels.js
@@ -22,46 +22,46 @@ let app2 = new Vue(
             {
                 name: 'Absolute Value',
                 description: "Vice President of international 501(c)(3) that hosts middle-school math competitions in over 20 chapters across 5 countries and 12 states.",
-                link: 'https://absval.org/'
+                link: 'absval.org/'
             },
             {
                 name: 'Knot Theory Research',
-                description: "Abstract: In knot theory, a standard crossing in a knot diagram is where one strand passes over another. In 2012, Adams introduced $n$-crossing projections, which are knot diagrams where each crossing consists of $n$ intersecting strands. The $n$-crossing number of a knot $K$, denoted by $c_n(K)$, is defined as the smallest number of crossings a $n$-crossing projection of $K$ can contain. Inequalities between the $2$-, $3$-, $4$-, and $5$-crossing numbers have been previously established. We prove $c_9(K)\leq c_3(K)-2$ for all knots $K$ that are not the trivial, trefoil, or figure-eight knot. We then generalize this result to prove that $c_{13}(K) < c_{5}(K)$ for a certain set of classes of $5$-crossing knots. Finally, we examine the remaining classes of $5$-crossing knots for possible counter-examples.",
+                description: "In the study of n-crossing knot diagrams, only inequalities for the 2-, 3-, 4-, and 5- crossing number had been known. Discovered and proved the 5th such inequality. Research advised by Colin Adams of Williams College.",
             },
             {
                 name: 'Princeton Speech & Debate',
                 description: "Competes on Princeton's Varsity Public Forum debate team. 3rd place at New Jersey’s 2022 State Championships.",
-                link: 'sites.google.com/view/phsmathteam/home'
+                link: 'phsdbteam.com/'
             },
             {
                 name: 'Ross Mathematics Program',
                 description: "Selected as a Junior Counselor at the Ross Mathematics Program, the oldest summer math program for high-schoolers in the US. Self-explored number theory and ramsey theory, graded participant sets, and lead problem review sessions.",
-                link: 'https://rossprogram.org/'
+                link: 'rossprogram.org/'
             },
             {
                 name: 'PHS Competitive Programming Team',
                 description: "Co-founder. Teaches competitive programming to members and competes in national tournaments. Won 2nd place at Lockheed Martin's CodeQuest competition.",
-                link: 'https://phscpt.github.io/'
+                link: 'phscpt.github.io/'
             },
             {
                 name: 'NJ Governor\'s STEM Scholars Program',
                 description: "Selected as the one of the top 100 STEM students in New Jersey. Currently completing a year-long research project in the program.",
-                link: 'https://www.govstemscholars.com/'
+                link: 'govstemscholars.com/'
             },
             {
                 name: 'Ideas Center',
                 description: "Leader on the Ideas Center Board and volunteer tutor at Princeton High School tutoring center. March 2022 \“Tutor of the Month.\”",
-                link: 'https://www.princetonk12.org/princeton-high-school/students/ideas-center'
+                link: 'princetonk12.org/princeton-high-school/students/ideas-center'
             },
             {
                 name: 'π & AI',
                 description: "Co-president of π & AI, which runs weekly math and programming circles at local middle school and library. Earned $600 cash funding from America's Promise Alliance.",
-                link: 'https://princetonlibrary.libnet.info/event/7252938'
+                link: 'princetonlibrary.libnet.info/event/7252938'
             },
             {
                 name: 'Princeton HR Insight LLC',
                 description: "Head of Marketing for Princeton HR Insight LLC. Create marketing videos viewed by thousands of customers and potential customers.",
-                link: 'https://www.princetonhrinsight.com/'
+                link: 'princetonhrinsight.com/'
             }
         ]
     }
@@ -73,11 +73,11 @@ let achievements = new Vue({
         items: [
             {
                 name: "Competitive Programming",
-                description: "2nd place at Lockheed Martin\'s CodeQuest<br>2nd place at University of Philidelphia\'s Philidelphia Classic",
+                description: "2nd place at Lockheed Martin\'s CodeQuest. 2nd place at University of Philidelphia\'s Philidelphia Classic",
             },
             {
                 name: "Hackathons",
-                description: "1st place at hackPHS 2021<br>Best in Data Visualization at PantherHack 2021",
+                description: "1st place at hackPHS 2021. Best in Data Visualization at PantherHack 2021",
             },
             {
                 name: "Writing",
@@ -85,7 +85,7 @@ let achievements = new Vue({
             },
             {
                 name: "Mathematics",
-                description: "Accepted to Program in Algebraic and Combinatorial Thinking",
+                description: "Accepted to present research at the 2023 Joint Mathematics Meetings. Accepted to Program in Algebraic and Combinatorial Thinking.",
             }
         ]
     }
